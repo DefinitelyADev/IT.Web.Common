9cec3603aa39765f212d334173e83b4871c747f7
Ilias Tsakiridis <ilias.tsakiridis@outlook.com>
2022-06-09 22:17:42 +0300

    Add route prefix convention

---
+++ src/IT.Web.Common/Extensions/MvcOptionsExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Credit: https://stackoverflow.com/a/58406404/4857852
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace IT.Web.Common.Extensions;
+
+public static class MvcOptionsExtensions
+{
+    /// <summary>
+    /// Adds a RoutePrefixConvention to the MvcOptions conventions
+    /// </summary>
+    /// <param name="options">The mvc options</param>
+    /// <param name="routeAttribute">The route template provider</param>
+    public static void AddGeneralRoutePrefix(this MvcOptions options, IRouteTemplateProvider routeAttribute)
+    {
+        options.Conventions.Add(new RoutePrefixConvention(routeAttribute));
+    }
+
+    /// <summary>
+    /// Adds a RoutePrefixConvention to the MvcOptions conventions
+    /// </summary>
+    /// <param name="options">The mvc options</param>
+    /// <param name="prefixTemplate">The prefix template</param>
+    public static void AddGeneralRoutePrefix(this MvcOptions options, string prefixTemplate)
+    {
+        options.AddGeneralRoutePrefix(new RouteAttribute(prefixTemplate));
+    }
+}
---
+++ src/IT.Web.Common/RoutePrefixConvention.cs
@@ -0,0 +1,23 @@
+﻿// Credit: https://stackoverflow.com/a/58406404/4857852
+
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace IT.Web.Common;
+
+public class RoutePrefixConvention : IApplicationModelConvention
+{
+    private readonly AttributeRouteModel _routePrefix;
+
+    public RoutePrefixConvention(IRouteTemplateProvider route) => _routePrefix = new AttributeRouteModel(route);
+
+    public void Apply(ApplicationModel application)
+    {
+        foreach (SelectorModel? selector in application.Controllers.SelectMany(c => c.Selectors))
+        {
+            selector.AttributeRouteModel = selector.AttributeRouteModel != null
+                ? AttributeRouteModel.CombineAttributeRouteModel(_routePrefix, selector.AttributeRouteModel)
+                : _routePrefix;
+        }
+    }
+}
